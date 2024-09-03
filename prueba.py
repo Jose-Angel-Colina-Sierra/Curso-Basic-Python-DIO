b83f5e2d0c72af13752e583619ed9051c718b51f
Jose Colina <ellocojoseangelcolinasierra07@gmail.com>
2024-09-03 12:01:20 -0300

    Modificacao da estrutura dos documentos.

--- prueba.py
+++ prueba.py
@@ -1,9 +1,1 @@
-texto = input("informe um texto: ")
-VOGAIS = "AEIOU"
 
-for letra in texto:
-    if letra.upper() in VOGAIS:
-        print(letra, end="")
-else:
-    print()
-    print("Executa no final do lazo")
