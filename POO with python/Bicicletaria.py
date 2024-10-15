a3cd8ea77ec51f80afc2463874ad6edc25ddf329
Jose Angel Colina Sierra <ellocojoseangelcolinasierra07@gmail.com>
2024-10-15 00:01:02 -0300

    Commit de atualizacao

--- POO with python/Bicicletaria.py
+++ POO with python/Bicicletaria.py
@@ -4,7 +4,7 @@ class Bicicleta():
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
-        
+
 
     def buzinar(seld):
         print("Piiiiii piiiiii!!!")
@@ -16,12 +16,17 @@ class Bicicleta():
     def correr(self):
         print("Vrummmmm...")
 
+    
+    def __str__(self):
+        return f"{self.__class__.__name__}: {[f'{chave}={valor}' for chave, valor in self.__dict__.items()]}"
+
 
 bicicleta1 = Bicicleta("marron","Praia",2018,3000)
 
-Bicicleta.correr(bicicleta1) # -> E o mesmo, funciona para referenciar o mesmo objeto (self)
-bicicleta1.parar()           # -> E o mesmo, funciona para referenciar o mesmo objeto (self)
-bicicleta1.buzinar()
-print(bicicleta1.cor,bicicleta1.ano,bicicleta1.modelo,bicicleta1.valor)
+# Bicicleta.correr(bicicleta1) # -> E o mesmo, funciona para referenciar o mesmo objeto (self)
+# bicicleta1.parar()           # -> E o mesmo, funciona para referenciar o mesmo objeto (self)
+# bicicleta1.buzinar()
+# print(bicicleta1.cor,bicicleta1.ano,bicicleta1.modelo,bicicleta1.valor)
 
-print("cambie")
+
+print(bicicleta1)
