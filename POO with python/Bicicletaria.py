066bf47658ebcffed774666487e54c7be4d8073c
Jose Angel Colina Sierra <ellocojoseangelcolinasierra07@gmail.com>
2024-10-16 20:00:23 -0300

    atualizando o curso 2

--- POO with python/Bicicletaria.py
+++ POO with python/Bicicletaria.py
@@ -1,24 +1,26 @@
 class Bicicleta():
-    def __init__(self,cor,modelo,ano, valor):
+    def __init__(self, cor, modelo, ano, valor):
         self.cor = cor
         self.modelo = modelo
         self.ano = ano
         self.valor = valor
+        
 
 
     def buzinar(seld):
         print("Piiiiii piiiiii!!!")
 
     def parar(self):
+        
         print("Parando bicicleta...")
         print("Bicicleta parada!")
 
     def correr(self):
         print("Vrummmmm...")
 
-    
+
     def __str__(self):
-        return f"{self.__class__.__name__}: {[f'{chave}={valor}' for chave, valor in self.__dict__.items()]}"
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
 
 
 bicicleta1 = Bicicleta("marron","Praia",2018,3000)
